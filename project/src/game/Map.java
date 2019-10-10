42423cd7c67778af51c32fbfd917d976401abdea
hanum-lee <chococloudy@gmail.com>
2019-10-10 17:01:09 -0600

    map class, text maze changed. switch case to array

--- project/src/game/Map.java
+++ project/src/game/Map.java
@@ -86,66 +86,74 @@ public class Map
 		final int COLS = 20;
 		String[][] maze = getMaze();
 		String[][] textMaze = new String[ROWS][COLS];
+		String[] letterArray = {"w","v","r","x","1","2","3","4","5","6","a","b","c","d","e","n","m"};
+		String[] corresArray = {"#","#"," ","X","?","?","?","?","?","?","E","E","E","E","E"," "," "};
+
 		for(int r = 0; r < ROWS; r++) 
 		{
 			for(int c = 0; c < COLS; c++) 
 			{
 				String letter = maze[r][c];
 
-				switch(letter)
-				{
-				case "w":
-					textMaze[r][c] = "#";
-					break;
-				case "v":
-					textMaze[r][c] = "#";
-					break;
-				case "r":
-					textMaze[r][c] = " ";
-					break;
-				case "x":
-					textMaze[r][c] = "X";
-					break;
-				case "1":
-					textMaze[r][c] = "?";
-					break;
-				case "2":
-					textMaze[r][c] = "?";
-					break;
-				case "3":
-					textMaze[r][c] = "?";
-					break;
-				case "4":
-					textMaze[r][c] = "?";
-					break;
-				case "5":
-					textMaze[r][c] = "?";
-					break;
-				case "6":
-					textMaze[r][c] = "?";
-					break;
-				case "a":
-					textMaze[r][c] = "E";
-					break;
-				case "b":
-					textMaze[r][c] = "E";
-					break;
-				case "c":
-					textMaze[r][c] = "E";
-					break;
-				case "d":
-					textMaze[r][c] = "E";
-					break;
-				case "e":
-					textMaze[r][c] = "E";
-					break;
-				case "n":
-					textMaze[r][c] = " ";
-					break;
-				case "m":
-					textMaze[r][c] = " ";
-					break;
+				int indexofLetter = java.util.Arrays.asList(letterArray).indexOf(letter);
+				if(indexofLetter > 0){
+					textMaze[r][c] = corresArray[indexofLetter];
 				}
+//
+//				switch(letter)
+//				{
+//				case "w":
+//					textMaze[r][c] = "#";
+//					break;
+//				case "v":
+//					textMaze[r][c] = "#";
+//					break;
+//				case "r":
+//					textMaze[r][c] = " ";
+//					break;
+//				case "x":
+//					textMaze[r][c] = "X";
+//					break;
+//				case "1":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "2":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "3":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "4":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "5":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "6":
+//					textMaze[r][c] = "?";
+//					break;
+//				case "a":
+//					textMaze[r][c] = "E";
+//					break;
+//				case "b":
+//					textMaze[r][c] = "E";
+//					break;
+//				case "c":
+//					textMaze[r][c] = "E";
+//					break;
+//				case "d":
+//					textMaze[r][c] = "E";
+//					break;
+//				case "e":
+//					textMaze[r][c] = "E";
+//					break;
+//				case "n":
+//					textMaze[r][c] = " ";
+//					break;
+//				case "m":
+//					textMaze[r][c] = " ";
+//					break;
+//				}
 			}
 		}
 		return textMaze;
